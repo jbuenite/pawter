337148b2d9aafbb70beb4ff4faee6b10419d544a
jbuenite <123060631+jbuenite@users.noreply.github.com>
2023-02-02 12:16:38 +0100

    Add files via upload

---
+++ Biblioteca/Libro.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programación.Tema6.Biblioteca;
+
+/**
+ *
+ * @author Juan Instituto
+ */
+public class Libro {
+    
+}
